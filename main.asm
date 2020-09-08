0e1297e9e1376d9d524cc0f9b108bd6284599045
lightda <18072976+lightda104530@users.noreply.github.com>
2020-09-08 19:39:29 +0800

    format change.

--- main.asm
+++ main.asm
@@ -5,30 +5,33 @@
 		mov sp, bp
 	
 		call switch_to_pm
-			jmp $
+			
+		jmp $
  
 	%include 'gdt.asm'
  
 		switch_to_pm:
 			cli
-				lgdt [gdt_descriptor]
-					mov eax, cr0
-						or eax, 0x1
-							mov cr0, eax
-								jmp KERNEL_CODE_SEG:pm_switched
+			lgdt [gdt_descriptor]
+			mov eax, cr0
+			or eax, 0x1
+			mov cr0, eax
+			
+		jmp KERNEL_CODE_SEG:pm_switched
  
 	[bits 32]
  
 		pm_switched:
 			mov ax, KERNEL_DATA_SEG
-				mov ds, ax
-					mov ss, ax
-						mov es, ax
-							mov fs, ax
-								mov gs, ax
-									mov ebp, 0x90000
-										mov esp, ebp
-											jmp $
+			mov ds, ax
+			mov ss, ax
+			mov es, ax
+			mov fs, ax
+			mov gs, ax
+			mov ebp, 0x90000
+			mov esp, ebp
+			
+		jmp $
  
 	times 510-($-$$ ) db 0
 	dw 0xaa55
